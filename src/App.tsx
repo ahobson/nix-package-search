dea3361045a1eb82d5698579a5e1941fb276d427
Andrew Hobson <ahobson@truss.works>
2021-03-19 15:16:51 -0400

    First empty version

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Nix Package Search</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
