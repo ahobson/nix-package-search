fc7ee33942a4e5abaf4da686758e765de57d590b
Andrew Hobson <ahobson@gmail.com>
2021-09-30 21:03:06 -0400

    Not working jest config

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from '@jest/types'
+
+// Sync object
+const config: Config.InitialOptions = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  //preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {},
+}
+export default config
