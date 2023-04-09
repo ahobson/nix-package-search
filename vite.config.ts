76acff6008c0531e472261834971cd094683a07d
Andrew Hobson <ahobson@gmail.com>
2023-04-08 22:50:45 -0400

    Setting base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nix-package-search/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
