ea18b45d106413f3ad961bcfbb04ae14fb1e902b
Andrew Hobson <ahobson@gmail.com>
2023-08-20 21:55:29 -0400

    Use mui/system upgrade react

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-// taken from
-// https://github.com/mui-org/material-ui/issues/13394#issuecomment-815452717
-
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
-
-import { createTheme } from '@mui/material/styles'
-import { unstable_createMuiStrictModeTheme } from '@mui/material'
-const myCreateTheme = import.meta.env.PROD
-  ? createTheme
-  : unstable_createMuiStrictModeTheme
-const theme = myCreateTheme({})
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({})
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
