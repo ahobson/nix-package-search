6f0b9226153018906f5c6a2be658e21a7b6684d8
Andrew Hobson <ahobson@gmail.com>
2025-10-30 21:13:35 -0400

    YOLO see if chunked works

--- src/config/worker.ts
+++ src/config/worker.ts
@@ -2,6 +2,7 @@
 // jest doesn't handle import.meta yet
 
 import { createDbWorker } from 'sql.js-httpvfs'
+import { SplitFileConfigInner } from 'sql.js-httpvfs/dist/sqlite.worker'
 
 // sadly there's no good way to package workers and wasm directly so
 // you need a way to get these two URLs from your bundler.
@@ -14,17 +15,43 @@ const workerUrl = new URL(
 const wasmUrl = new URL('sql.js-httpvfs/dist/sql-wasm.wasm', import.meta.url)
 
 const publicPath = import.meta.env.BASE_URL || ''
-const gitSha = import.meta.env.VITE_GITHUB_SHA || 'local'
+const gitSha: string = import.meta.env.VITE_GITHUB_SHA || 'local'
+const r = await fetch(
+  `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`,
+  {
+    method: 'HEAD',
+  }
+)
+
+const contentLength = parseInt(r.headers.get('content-length') || '0')
+
+const config: SplitFileConfigInner = (() => {
+  const url = `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`
+  if (contentLength > 0) {
+    const c: SplitFileConfigInner = {
+      requestChunkSize: 4096,
+      cacheBust: gitSha,
+      serverMode: 'chunked', // file is just a plain old full sqlite database
+      urlPrefix: url,
+      databaseLengthBytes: contentLength,
+      serverChunkSize: contentLength,
+      suffixLength: 0,
+    }
+    return c
+  }
+  return {
+    serverMode: 'full',
+    requestChunkSize: 4096,
+    url,
+    cacheBust: gitSha,
+  }
+})()
+
 const dbWorker = createDbWorker(
   [
     {
       from: 'inline',
-      config: {
-        serverMode: 'full', // file is just a plain old full sqlite database
-        requestChunkSize: 4096,
-        url: `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`,
-        cacheBust: gitSha,
-      },
+      config,
     },
   ],
   workerUrl.toString(),
