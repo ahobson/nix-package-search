f32284ca00073b07926b4c1592b31369ab8fcd34
Andrew Hobson <ahobson@gmail.com>
2025-11-15 21:42:15 -0500

    convert to sqlite-wasm-http

--- src/config/worker.ts
+++ src/config/worker.ts
@@ -1,63 +1,36 @@
 // put these in their own file so they can be mocked for testing
 // jest doesn't handle import.meta yet
 
-import { createDbWorker } from 'sql.js-httpvfs'
-import { SplitFileConfigInner } from 'sql.js-httpvfs/dist/sqlite.worker'
-
-// sadly there's no good way to package workers and wasm directly so
-// you need a way to get these two URLs from your bundler.
-// This is the webpack5 way to create a asset bundle of the worker and wasm:
-const workerUrl = new URL(
-  'sql.js-httpvfs/dist/sqlite.worker.js',
-  import.meta.url
-)
-
-const wasmUrl = new URL('sql.js-httpvfs/dist/sql-wasm.wasm', import.meta.url)
+import { createSQLiteThread, createHttpBackend } from 'sqlite-wasm-http'
 
 const publicPath = import.meta.env.BASE_URL || ''
-const gitSha: string = import.meta.env.VITE_GITHUB_SHA || 'local'
-const r = await fetch(
-  `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`,
-  {
-    method: 'HEAD',
-  }
-)
+const remoteURL = `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`
 
-const contentLength = parseInt(r.headers.get('content-length') || '0')
+const httpBackend = createHttpBackend({
+  maxPageSize: 1024, // this is the current default SQLite page size
+  timeout: 10000, // 10s
+  cacheSize: 4096, // 4 MB
+})
 
-const config: SplitFileConfigInner = (() => {
-  const url = `${publicPath}nix/nixpkgs-unstable/all_packages.sqlite3`
-  if (contentLength > 0) {
-    const c: SplitFileConfigInner = {
-      requestChunkSize: 4096,
-      cacheBust: gitSha,
-      serverMode: 'chunked',
-      urlPrefix: url,
-      databaseLengthBytes: contentLength,
-      serverChunkSize: contentLength,
-      suffixLength: 0,
-    }
-    return c
-  }
-  return {
-    serverMode: 'full', // file is just a plain old full sqlite database
-    requestChunkSize: 4096,
-    url,
-    cacheBust: gitSha,
-  }
-})()
-
-const dbWorker = createDbWorker(
-  [
-    {
-      from: 'inline',
-      config,
+export async function query(sql: string, bind: object): Promise<any[]> {
+  const db = await createSQLiteThread({ http: httpBackend })
+  // This API is compatible with all SQLite VFS
+  await db('open', { filename: 'file:' + encodeURI(remoteURL), vfs: 'http' })
+  const rows: any[] = []
+  await db('exec', {
+    sql,
+    bind,
+    rowMode: 'object',
+    callback: (msg: any) => {
+      if (msg.row) {
+        rows.push(msg.row)
+      }
     },
-  ],
-  workerUrl.toString(),
-  wasmUrl.toString()
-)
+  })
+  // This closes the DB connection
+  await db('close', {})
+  db.close()
+  await httpBackend.close()
 
-export function getDbWorker() {
-  return dbWorker
+  return rows
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
+import wasm from 'vite-plugin-wasm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/nix-package-search/',
-  plugins: [svgLoader()],
+  plugins: [svgLoader(), wasm()],
+  worker: {
+    format: 'es',
+  },
   test: {
     environment: 'jsdom',
     //   setupFiles: ['./src/setupTests.ts'],
